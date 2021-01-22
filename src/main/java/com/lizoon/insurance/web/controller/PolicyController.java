a58425fe389c2cf320812a14ff8cc7fe8bae0212
liza <liza.shpinkovaa@mail.ru>
2021-01-22 15:24:17 +0300

    Fixed xml view

--- src/main/java/com/lizoon/insurance/web/controller/PolicyController.java
+++ src/main/java/com/lizoon/insurance/web/controller/PolicyController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/policy")
+@RequestMapping("/")
 @RequiredArgsConstructor
 public class PolicyController {
 
